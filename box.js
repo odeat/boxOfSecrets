bab3ee7d9688b5e473a85d8779f6be6f1e43449e
odeat <dua2003@live.nl>
2025-05-26 14:08:35 +0200

    een heleboel changes

--- box.js
+++ box.js
@@ -1,13 +1,36 @@
 class Box {
   constructor(x, y) {
+    this.w = 50;
+    this.h = 50;
     this.particle = new toxi.physics2d.VerletParticle2D(x, y);
     physics.addParticle(this.particle);
-    this.locked = false;
   }
 
   display() {
-    fill(100);
-    noStroke();
-    ellipse(this.particle.x, this.particle.y, 20, 20);
+    fill(171,137,112); // brown color to represent a box xd
+    // noStroke();
+    rectMode(CENTER);
+    rect(this.particle.x, this.particle.y, this.w, this.h);
+  }
+
+  contains(px, py) {
+    return (
+      px > this.particle.x - this.w / 2 &&
+      px < this.particle.x + this.w / 2 &&
+      py > this.particle.y - this.h / 2 &&
+      py < this.particle.y + this.h / 2
+    );
+  }
+
+  lock() {
+    this.particle.lock();
+  }
+
+  unlock() {
+    this.particle.unlock();
+  }
+
+  moveTo(x, y) {
+    this.particle.set(x, y);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 let physics;
 let boxes = [];
 
+let grabbedBox = null;
+let grabOffset;
+
+
+function preload() {
+  boxImage = loadImage('assets/closedBox.png');
+}
+
 function setup() {
   createCanvas(800, 800);
   physics = new toxi.physics2d.VerletPhysics2D();
-  physics.setWorldBounds(new toxi.geom.Rect(0, 0, width, height));
-  physics.addBehavior(new toxi.physics2d.behaviors.GravityBehavior(new toxi.geom.Vec2D(0, 0.5)));
+  physics.setWorldBounds(new toxi.geom.Rect(0, 0, width - 25, height - 25));
+  physics.addBehavior(new toxi.physics2d.behaviors.GravityBehavior(new toxi.geom.Vec2D(0, 0.4))); // zwaartekracht element
 
   // create some "objects"
   for (let i = 0; i < 10; i++) {
@@ -23,6 +31,30 @@ function draw() {
 // ---------- RENDER SECTION ----------
   for (let box of boxes) {
     box.display();
+  }
+}
+
+function mousePressed() {
+  for (let box of boxes) {
+    if (box.contains(mouseX, mouseY)) {
+      grabbedBox = box;
+      grabOffset = createVector(mouseX - box.particle.x, mouseY - box.particle.y);
+      grabbedBox.lock();
+      break;
+    }
+  }
+}
+
+function mouseDragged() {
+  if (grabbedBox) {
+    grabbedBox.moveTo(mouseX - grabOffset.x, mouseY - grabOffset.y);
+  }
+}
+
+function mouseReleased() {
+  if (grabbedBox) {
+    grabbedBox.unlock();
+    grabbedBox = null;
   }
 }
 
